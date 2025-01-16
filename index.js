84fea0f89168462e7bd2f90b16c3f4653735bed5
Yash <yashvardhansingh232@gmail.com>
2025-01-17 01:57:40 +0530

    modified index.js

--- index.js
+++ index.js
@@ -1,21 +1,3 @@
-// Project: User Authentication System
-
-// 1. Developed a robust HTTP server using Express.js in Node.js, 
-// implementing secure user authentication through JWT tokens.
-
-// 2. Designed APIs to handle user registration and login with GET and 
-// POST requests, validating inputs using the Zod library for enhanced 
-// data integrity.
-
-// 3. Conducted thorough testing of the APIs using Postman to ensure 
-// functionality and reliability.
-
-// 4. Implemented global error handling to provide user-friendly error 
-// messages and employed rate limiting to mitigate abuse and ensure fair 
-// usage.
-
-// 5. Utilized MongoDB for efficient storage and retrieval of user data 
-// and metadata to enhance application performance and scalability.
 
 const express = require("express");
 const zod = require("zod");
